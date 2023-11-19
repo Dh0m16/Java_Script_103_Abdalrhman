abc121fd0d5012ce03f2e291086ae1e5e9130230
Dh0m16 <144840435+Dh0m16@users.noreply.github.com>
2023-11-19 15:41:08 +0300

    .

---
+++ Regular Expressions/Replace.js
@@ -0,0 +1,4 @@
+const myString = 'I Love JavaScript'
+const myRegex = /JavaScript/
+const newString = myString.replace(myRegex)
+console.log(newString)
---
+++ Regular Expressions/Search.js
@@ -0,0 +1,4 @@
+const myString = 'I Love JavaScript'
+
+let newRegex = myString.search(/JavaScript/)
+console.log(newRegex)
